a4c045c4e9c8749fb73d66d0d0f482c34f943d91
Kszymhu <czolczynski.k@gmail.com>
2023-01-21 22:55:58 +0100

    I should be studying rn

--- zadanie-220123/main.py
+++ zadanie-220123/main.py
@@ -1,28 +1,58 @@
 import pandas as pd
 
-FOOD_DATA_FILENAME = 'sampledatafoodinfo.csv'
+INPUT_DATA_FILENAME = 'sampledatafoodinfo.csv'
+CLEAN_DATA_FILENAME = 'cleandata.csv'
+EXTENDED_DATA_FILENAME = 'extendeddata.csv'
+LOW_CARB_FOODS_FILENAME = 'lowcarbfoods.csv'
 
 
-def load_food_data(filename: str) -> pd.DataFrame:
-    """
-    Reads data from `filename`.csv file, assuming that it has the same columns as sampledatafoodinfo.csv.
-    Also changes nans into 0s.
-    """
+def load_and_parse_data(filename: str) -> pd.DataFrame:
+    return pd.read_csv(filename, sep=';')
 
-    dataframe_raw: pd.DataFrame = pd.read_csv(filename, sep=';')
-    dataframe_clean: pd.DataFrame = dataframe_raw.iloc[:, :-2]  # drop last two columns
 
-    rows = [x[1] for x in dataframe_clean.iterrows()]  # my god, I hate pandas
-
-    for row in rows:
-        row: pd.Series
-        columns = row.axes[0].values
-        for column in columns:
-            if row[column] == 'nan':
-                row[column] = 0.0
-
+def clean_up_data(raw_data: pd.DataFrame) -> pd.DataFrame:
+    dataframe_clean: pd.DataFrame = raw_data.iloc[:, :-2]  # drop last two columns (empty and notes)
+    dataframe_clean['Fibre'] = dataframe_clean['Fibre'].str.replace(',', '.').astype(float)
+    # ^ Fibre column uses different decimal separators.
+    dataframe_clean.fillna(0.0, inplace=True)  # change NaNs to 0s
     return dataframe_clean
 
 
+def get_carb_calorie_content(data: pd.DataFrame, index: int) -> float:
+    row = data.iloc[index]
+    calories = row['Calories']
+    carb_calories = row['Carbs'] * 4  # 1 g of carbs has 4 kcal
+
+    if calories == 0 or carb_calories == 0:
+        return 0
+
+    carb_calorie_content = carb_calories / calories
+
+    return carb_calorie_content
+
+
+def get_extended_dataframe(data: pd.DataFrame) -> pd.DataFrame:
+    """Creates a dataframe with 'Carb Calorie Content' column"""
+    row_count = len(data.index)
+    carb_calorie_content_column = [get_carb_calorie_content(data, x) for x in range(row_count)]
+    data['Carb Calorie Content'] = carb_calorie_content_column
+    return data
+
+
+def get_low_carb_foods_dataframe(data: pd.DataFrame, threshold: float):
+    result = data[data['Carb Calorie Content'] <= threshold]
+    return result
+
+
 if __name__ == '__main__':
-    data = load_food_data(FOOD_DATA_FILENAME)
+    raw_data = load_and_parse_data(INPUT_DATA_FILENAME)
+    clean_data = clean_up_data(raw_data)
+    clean_data.to_csv(CLEAN_DATA_FILENAME, index=False)
+
+    extended_data = get_extended_dataframe(clean_data)
+    extended_data.to_csv(EXTENDED_DATA_FILENAME, index=False)
+
+    low_carb_foods = get_low_carb_foods_dataframe(extended_data, .5)
+    low_carb_foods.to_csv(LOW_CARB_FOODS_FILENAME, index=False)
+
+

9dde6b9e205a663b6c2d6ff784642ac0cdc4b79d
Kszymhu <czolczynski.k@gmail.com>
2023-01-21 19:31:51 +0100

    Add misc.xml

--- zadanie-220123/main.py
+++ zadanie-220123/main.py
@@ -1,1 +1,28 @@
+import pandas as pd
 
+FOOD_DATA_FILENAME = 'sampledatafoodinfo.csv'
+
+
+def load_food_data(filename: str) -> pd.DataFrame:
+    """
+    Reads data from `filename`.csv file, assuming that it has the same columns as sampledatafoodinfo.csv.
+    Also changes nans into 0s.
+    """
+
+    dataframe_raw: pd.DataFrame = pd.read_csv(filename, sep=';')
+    dataframe_clean: pd.DataFrame = dataframe_raw.iloc[:, :-2]  # drop last two columns
+
+    rows = [x[1] for x in dataframe_clean.iterrows()]  # my god, I hate pandas
+
+    for row in rows:
+        row: pd.Series
+        columns = row.axes[0].values
+        for column in columns:
+            if row[column] == 'nan':
+                row[column] = 0.0
+
+    return dataframe_clean
+
+
+if __name__ == '__main__':
+    data = load_food_data(FOOD_DATA_FILENAME)
